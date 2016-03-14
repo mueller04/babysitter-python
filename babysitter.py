e9d3146c343c9ab4bf51865209eeb9e5bdc73102
Mike Mueller <mikemueller04@hotmail.com>
2016-03-14 14:45:46 -0400

    passed test_paid_12hr_start_to_endtime

--- babysitter.py
+++ babysitter.py
@@ -1,13 +1,15 @@
+START_TIME_RATE = 12
+
 class Babysitter():
 
-    def calculate(self, start_time):
-        isValidTime = self.validateTime(start_time)
+    def calculate(self, start_time, end_time):
+        isValidTime = self.validateTime(start_time, end_time)
         if isValidTime == None:
-            return 60;
+            return START_TIME_RATE * (end_time - start_time);
         else:
             return isValidTime
 
-    def validateTime(self, start_time):
+    def validateTime(self, start_time, end_time):
         if start_time < 17 and start_time > 3:
             return "start time cannot be before 5pm or after 3am"
         else:
--- babysittertest.py
+++ babysittertest.py
@@ -7,13 +7,16 @@ class BabysitterTest(unittest.TestCase):
         self.babysitter = Babysitter()
 
     def test_starttime_cannot_be_before_5pm(self):
-        self.assertEqual("start time cannot be before 5pm or after 3am", Babysitter.calculate(self.babysitter, 16))
+        self.assertEqual("start time cannot be before 5pm or after 3am", Babysitter.calculate(self.babysitter, 16, 20))
 
     def test_starttime_cannot_be_after_3am(self):
-        self.assertEqual("start time cannot be before 5pm or after 3am", Babysitter.calculate(self.babysitter, 4))
+        self.assertEqual("start time cannot be before 5pm or after 3am", Babysitter.calculate(self.babysitter, 4, 20))
 
     def test_valid_starttime(self):
-        self.assertNotEqual("start time cannot be before 5pm or after 3am", Babysitter.calculate(self.babysitter, 17))
+        self.assertNotEqual("start time cannot be before 5pm or after 3am", Babysitter.calculate(self.babysitter, 17, 20))
 
     def test_paid_12hr_start_to_bedtime(self):
-        self.assertEqual(60, Babysitter.calculate(self.babysitter, 17))
+        self.assertEqual(60, Babysitter.calculate(self.babysitter, 17, 22))
+
+    def test_paid_12hr_start_to_endtime(self):
+        self.assertEqual(48, Babysitter.calculate(self.babysitter, 17, 21))
